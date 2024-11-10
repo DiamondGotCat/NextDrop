b31a0a0469f12c7fa06e464f0cad3ccea609f2b9
DiamondGotCat <chii.2019.5555@gmail.com>
2024-11-11 02:05:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-APP = ['NextDrop.py'] 
+APP = ['NextDrop.py']
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    'includes': ['PyQt6', 'asyncio', 'aiohttp', 'concurrent.futures'],
-    'excludes': ['packaging'],  # 衝突するライブラリを除外
-    'packages': ['aiohttp', 'asyncio', 'concurrent', 'PyQt6'],
+    'includes': [
+        'kamu-jp-modern',
+        'PyQt6',
+        'asyncio',
+        'aiohttp',
+        'aiohttp.web',
+        'socket',
+        'os',
+        'sys',
+        'gzip',
+        'argparse',
+        'concurrent.futures',
+        'tqdm',
+        'io',
+        # 必要な他のモジュールをここに追加
+    ],
+    'excludes': ['packaging'],
+    'packages': find_packages(),
+    'plist': {
+        'CFBundleName': 'NextDrop',
+        'CFBundleDisplayName': 'NextDrop',
+        'CFBundleGetInfoString': "NextDrop Application",
+        'CFBundleIdentifier': "com.yourname.nextdrop",
+        'CFBundleVersion': "0.1.0",
+        'CFBundleShortVersionString': "0.1.0",
+        'NSHighResolutionCapable': True,
+    },
 }
 
 setup(
